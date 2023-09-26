ab24e15e1e832caedbb000811dd03bbd9445ef8b
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-09-27 02:13:30 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function Menubar() {
   return (
     <div className="menu-bar">
-      <img alt="Company Logo" height="30" src="./logo.png" width="30" />
+      <img alt="App Logo" height="30" src="./logo.png" width="30" />
       <h1 className="text-lg font-medium" style={{ textAlign: "center" }}>
         Ploio
       </h1>
@@ -40,6 +40,25 @@ function Menubar() {
           <path d="M4 22v-20h16v11.543c0 4.107-6 2.457-6 2.457s1.518 6-2.638 6h-7.362zm18-7.614v-14.386h-20v24h10.189c3.163 0 9.811-7.223 9.811-9.614zm-5-1.386h-8v-1h8v1zm0-4h-8v1h8v-1zm0-3h-8v1h8v-1zm-9 0h-1v1h1v-1zm0 3h-1v1h1v-1zm0 3h-1v1h1v-1z" />{" "}
         </svg>
         Summary
+      </a>
+      <a href="#">
+        <svg
+          fill="black"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.5"
+          viewBox="3 0 21 21"
+          width="30"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m2.095 19.886 9.248-16.5c.133-.237.384-.384.657-.384.272 0 .524.147.656.384l9.248 16.5c.064.115.096.241.096.367 0 .385-.309.749-.752.749h-18.496c-.44 0-.752-.36-.752-.749 0-.126.031-.252.095-.367zm1.935-.384h15.939l-7.97-14.219zm7.972-6.497c-.414 0-.75.336-.75.75v3.5c0 .414.336.75.75.75s.75-.336.75-.75v-3.5c0-.414-.336-.75-.75-.75zm-.002-3c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"
+            fill-rule="nonzero"
+          />{" "}
+        </svg>
+        Notice
       </a>
       <div className="bottom-items">
         <a href="#">
