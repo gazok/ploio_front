4b612d0bf2deba4c852880d6bc48af86cae180e7
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-10-01 10:39:56 +0900

    Update App.js
    메뉴 스위치 구현
    
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
+//App.js
+//react-router-dom@5.3.4
+
+import React from "react";
 import "./App.css";
+import Summary from "./Summary";
+import Notice from "./Notice";
+import Profile from "./Profile";
+import Setting from "./Setting";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 //create menu
 function Menubar() {
   return (
     <div className="menu-bar">
-      <img alt="App Logo" height="30" src="./logo.png" width="30" />
+      <img alt="App Logo" height="30" src="./logo.svg" width="30" />
       <h1 className="text-lg font-medium" style={{ textAlign: "center" }}>
         Ploio
       </h1>
-      <a href="#">
+      <Link to="/Content">
         <svg
           fill="none"
           height="24"
@@ -24,8 +34,8 @@ function Menubar() {
           <polyline points="9 22 9 12 15 12 15 22" />
         </svg>
         Home
-      </a>
-      <a href="#">
+      </Link>
+      <Link to="/Summary">
         <svg
           fill="black"
           height="24"
@@ -40,8 +50,8 @@ function Menubar() {
           <path d="M4 22v-20h16v11.543c0 4.107-6 2.457-6 2.457s1.518 6-2.638 6h-7.362zm18-7.614v-14.386h-20v24h10.189c3.163 0 9.811-7.223 9.811-9.614zm-5-1.386h-8v-1h8v1zm0-4h-8v1h8v-1zm0-3h-8v1h8v-1zm-9 0h-1v1h1v-1zm0 3h-1v1h1v-1zm0 3h-1v1h1v-1z" />{" "}
         </svg>
         Summary
-      </a>
-      <a href="#">
+        </Link>
+      <Link to="/Notice">
         <svg
           fill="black"
           height="24"
@@ -59,9 +69,9 @@ function Menubar() {
           />{" "}
         </svg>
         Notice
-      </a>
+      </Link>
       <div className="bottom-items">
-        <a href="#">
+        <Link to="/Profile">
           <svg
             fill="none"
             height="24"
@@ -79,8 +89,8 @@ function Menubar() {
             <path d="M16 3.13a4 4 0 0 1 0 7.75" />
           </svg>
           Profile
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="/Setting">
           <svg
             fill="none"
             height="24"
@@ -95,7 +105,7 @@ function Menubar() {
             <path d="M 10.490234 2 C 10.011234 2 9.6017656 2.3385938 9.5097656 2.8085938 L 9.1757812 4.5234375 C 8.3550224 4.8338012 7.5961042 5.2674041 6.9296875 5.8144531 L 5.2851562 5.2480469 C 4.8321563 5.0920469 4.33375 5.2793594 4.09375 5.6933594 L 2.5859375 8.3066406 C 2.3469375 8.7216406 2.4339219 9.2485 2.7949219 9.5625 L 4.1132812 10.708984 C 4.0447181 11.130337 4 11.559284 4 12 C 4 12.440716 4.0447181 12.869663 4.1132812 13.291016 L 2.7949219 14.4375 C 2.4339219 14.7515 2.3469375 15.278359 2.5859375 15.693359 L 4.09375 18.306641 C 4.33275 18.721641 4.8321562 18.908906 5.2851562 18.753906 L 6.9296875 18.1875 C 7.5958842 18.734206 8.3553934 19.166339 9.1757812 19.476562 L 9.5097656 21.191406 C 9.6017656 21.661406 10.011234 22 10.490234 22 L 13.509766 22 C 13.988766 22 14.398234 21.661406 14.490234 21.191406 L 14.824219 19.476562 C 15.644978 19.166199 16.403896 18.732596 17.070312 18.185547 L 18.714844 18.751953 C 19.167844 18.907953 19.66625 18.721641 19.90625 18.306641 L 21.414062 15.691406 C 21.653063 15.276406 21.566078 14.7515 21.205078 14.4375 L 19.886719 13.291016 C 19.955282 12.869663 20 12.440716 20 12 C 20 11.559284 19.955282 11.130337 19.886719 10.708984 L 21.205078 9.5625 C 21.566078 9.2485 21.653063 8.7216406 21.414062 8.3066406 L 19.90625 5.6933594 C 19.66725 5.2783594 19.167844 5.0910937 18.714844 5.2460938 L 17.070312 5.8125 C 16.404116 5.2657937 15.644607 4.8336609 14.824219 4.5234375 L 14.490234 2.8085938 C 14.398234 2.3385937 13.988766 2 13.509766 2 L 10.490234 2 z M 12 8 C 14.209 8 16 9.791 16 12 C 16 14.209 14.209 16 12 16 C 9.791 16 8 14.209 8 12 C 8 9.791 9.791 8 12 8 z" />
           </svg>
           Setting
-        </a>
+        </Link>
       </div>
     </div>
   );
@@ -105,7 +115,7 @@ function Menubar() {
 function Top() {
   return (
     <nav className="top-bar">
-      <a href="#">
+      <Link to="#">
         <svg
           fill="none"
           height="24"
@@ -113,45 +123,50 @@ function Top() {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="1"
-          viewBox="0 0 23 23"
+          viewBox="0 -7 30 30"
           width="30"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M16 1c-4.418 0-8 3.582-8 8 0 .585.063 1.155.182 1.704l-8.182 7.296v5h6v-2h2v-2h2l3.066-2.556c.909.359 1.898.556 2.934.556 4.418 0 8-3.582 8-8s-3.582-8-8-8zm-6.362 17l3.244-2.703c.417.164 1.513.703 3.118.703 3.859 0 7-3.14 7-7s-3.141-7-7-7c-3.86 0-7 3.14-7 7 0 .853.139 1.398.283 2.062l-8.283 7.386v3.552h4v-2h2v-2h2.638zm.168-4l-.667-.745-7.139 6.402v1.343l7.806-7zm10.194-7c0-1.104-.896-2-2-2s-2 .896-2 2 .896 2 2 2 2-.896 2-2zm-1 0c0-.552-.448-1-1-1s-1 .448-1 1 .448 1 1 1 1-.448 1-1z" />
         </svg>
         Login
-      </a>
-      <a href="#">
+      </Link>
+      <Link to="#">
         <svg
           fill="black"
           height="24"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="1.5"
-          viewBox="2 0 23 23"
+          viewBox="2 -7 30 30"
           width="30"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M9.602 3.7c-1.154 1.937-.635 5.227 1.424 9.025.93 1.712.697 3.02.338 3.815-.982 2.178-3.675 2.799-6.525 3.456-1.964.454-1.839.87-1.839 4.004h-1.995l-.005-1.241c0-2.52.199-3.975 3.178-4.663 3.365-.777 6.688-1.473 5.09-4.418-4.733-8.729-1.35-13.678 3.732-13.678 3.321 0 5.97 2.117 5.97 6.167 0 3.555-1.949 6.833-2.383 7.833h-2.115c.392-1.536 2.499-4.366 2.499-7.842 0-5.153-5.867-4.985-7.369-2.458zm13.398 15.3h-3v-3h-2v3h-3v2h3v3h2v-3h3v-2z" />
         </svg>
         Singnup
-      </a>
+      </Link>
     </nav>
   );
 }
 
 //create content frame
 function Content() {
-  return <main className="content">Welcome to your dashboard</main>;
+  return <main className="content">Welcome to Ploio</main>;
 }
 
+//메뉴별로 페이지 변환
 function App() {
   return (
     <div className="app">
-      <Menubar />
-      <Top />
-      <Content />
+      <Router>
+        <Top />
+        <Menubar />
+        <Switch>
+          <Route path="/Summary" component={Summary} />
+          <Route path="/Notice" component={Notice} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/Setting" component={Setting} />
+          <Route path="/" component={Content} />
+        </Switch>
+      </Router>
     </div>
   );
 }
