63bb52b70bc7b98a82061c7f2407387dabbb7966
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-10-01 10:42:36 +0900

    Create Setting.js
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

---
+++ Setting.js
@@ -0,0 +1,14 @@
+//Setting.js
+
+import React from "react";
+import "./App.css";
+
+function Setting() {
+  return (
+    <div className="content">
+      <h3>Setting page</h3>
+    </div>
+  );
+}
+
+export default Setting;
