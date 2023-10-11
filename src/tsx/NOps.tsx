3d24deb69ac1ab221ea6a385ab9d4cc8ff8111c8
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-10-12 06:48:21 +0900

    Create NOps.tsx
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

---
+++ src/tsx/NOps.tsx
@@ -0,0 +1,16 @@
+//NOps.tsx 
+//Ops 부문 Notice, 1차 발표 이후 보완 예정
+
+
+import React from "react";
+import "../App.css";
+
+function NOps() {
+  return (
+    <div className="content">
+      <h3> <br></br>Operation page</h3>
+    </div>
+  );
+}
+
+export default NOps;
