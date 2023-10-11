7592e7adb5eddab7c94f5ebe367fb2a191095ccb
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-10-12 07:02:36 +0900

    Update Security.js
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

--- src/js/Security.js
+++ src/js/Security.js
@@ -1,13 +1,19 @@
-//Security.js
+//Operation.tsx
+//Sec 부문 visualization
 //수정중
 
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import "./App.css";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 import "./Summary.css";
+import { VscExport } from "react-icons/vsc"; //icon 정의
 
-function Security() {
-  const history = useHistory();
+
+interface SecurityProps {
+}
+
+const Security: React.FC<SecurityProps> = () => {
+  const navigate = useNavigate();
   const [showCircles, setShowCircles] = useState(true);
   const [showInfo, setShowInfo] = useState(false); // 선을 클릭했을 때 창을 보여주는 상태
 
@@ -59,7 +65,7 @@ function Security() {
           <line
             x1={rect1X + rect1Width}
             y1={rect1Y + rect1Height / 2}
-            x2={rect2X -50}
+            x2={rect2X - 50}
             y2={rect2Y + rect2Height / 2}
             className="line-style" // CSS 클래스 적용
             markerEnd="url(#arrowhead)"
@@ -72,25 +78,11 @@ function Security() {
       {showInfo && (
         <div className="info-box">
           <div className="info-content">
-          <button onClick={handleLineClick} className="info-top">
+            <button onClick={handleLineClick} className="info-top">
             {/*닫기 버튼*/}
-            <svg
-              fill="full"
-              height="20"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1"
-              viewBox="-1 0 25 25"
-              width="13"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 3.795l2.995-2.98 11.132 11.185-11.132 11.186-2.995-2.981 8.167-8.205-8.167-8.205zm18.04 8.205l-8.167 8.205 2.995 2.98 11.132-11.185-11.132-11.186-2.995 2.98 8.167 8.206z"
-              />
-            </svg>
+             <VscExport />
             <b>Details</b>
-          </button>
+            </button>
             {/* 내용 */}
             <p>정보 창</p>
           </div>
