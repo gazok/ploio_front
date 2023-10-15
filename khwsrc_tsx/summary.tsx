29c04e1b42c338460166304ce3a437bf09cd1200
Hyeong-Uk <kohowo1999@snu.ac.kr>
2023-10-15 14:31:25 +0900

    merged front & get json from server v3 from hyeonguk

--- khwsrc_tsx/summary.tsx
+++ khwsrc_tsx/summary.tsx
@@ -50,50 +50,66 @@ function SummaryMenu() {
 }
 
 function Logic() {
-    let jsonData = {};
+    let jsonData = {"data": []};
     let success = false;
 
     //'http://serverIp:Port/Path'
-    // 할 것: cors 완벽 해결법, json input 얻기, 
-    // 질문: multi-fetch 수행법과 그 결과
-    fetch('http://127.0.0.1:8000/summary/security', {
-        mode: 'no-cors',
+    // 할 것: json input 얻기, 
+    fetch('http://127.0.0.1:8000/summary/tmp', {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
-    }).then(response => response.json()).then(response => {
-        if(response.success){
-            jsonData = response;
+    })
+    .then(response => {
+        if(response.ok){
+            //alert("응답 성공함");
             success = true;
+        }
+        else{
+            //alert("응답 실패함");
+            success = false;
+        }
+        return response.json();}).then(response => {
+        if(success){
+            alert("응답 성공함");
+            jsonData = response;
+            
+            
+            let p1 = document.getElementById("ptest1");
+            if(p1){
+                p1.innerHTML = JSON.stringify(jsonData);
+                //p1.innerHTML = JSON.stringify(jsonData.data[1]);
+            }
+            console.log(jsonData);
         }
         else{
             alert("응답 실패함");
         }
     });
     /*.then(response => console.log(response)) */
-
-    return [jsonData, success];
+    //console.log(jsonData);
+    //console.log(success); //false
+    return {jsonData, success}; //Ignore this. This doesn't work well.
 }
 
-// Outlet이 뭘까? "그림판" 같은 건가?
-// 10초를 페이지 상에서 잴 지 아니면 앱 전체에서 재고 데이터 받고 그 데이터 참조해서 진행할지 고르기.
+// Outlet이 뭘까?
 function Representation() {
-    
-    let [jsonData, success] = Logic(); //10초 후에 Logic 재실행 및 반복.
+    let {jsonData, success} = Logic(); //10초 후에 Logic 재실행 및 반복.
+    //console.log(jsonData);
 
     useEffect(() => {
         let timer = setInterval(() => {
-            [jsonData, success] = Logic();
+            let {jsonData, success} = Logic();
         }, 10000);
 
-        return () => {clearTimeout(timer)}
-    }, [])
+        return () => {clearTimeout(timer)};
+    }, []);
     
-
-    if(true){ //success로 바꾸기
-        return (<div><Outlet /></div>)
+    if(true){ //success로 바꾸기?
+        //return (<div><Outlet /></div>)
+        return (<div><p id="ptest1"></p></div>);
     }
     else{
-        return (<div>route B</div>)
+        return (<div>route B</div>);
     }
 }
 
