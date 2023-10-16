ecf0d6c505411dd245418864ee5a3bf01abaf925
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-10-16 17:07:22 +0900

    Update Operating.tsx
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

--- src/tsx/Operating.tsx
+++ src/tsx/Operating.tsx
@@ -1,15 +1,14 @@
 //Operation.tsx
 //Ops 부문 가시화
-//수정 중
+//수정 중 
 
-import React, { useState } from 'react';
-import { XYPlot, MarkSeries, LineSeries } from 'react-vis';
+import React, { useState, useEffect } from 'react';
+import { XYPlot, MarkSeries, LineSeries  } from 'react-vis';
 import data from '../data.json';
-import '../App.css';
 import './Summary.css';
-import { VscExport } from 'react-icons/vsc';
+import { VscExport, VscCircleSmall } from 'react-icons/vsc';
 
-// 데이터 형식 정의
+// 데이터 인터페이스 정의
 interface Data {
   src_ip: string;
   src_port: number;
@@ -20,113 +19,90 @@ interface Data {
 }
 
 const Operation: React.FC = () => {
+  // UI 요소 관리를 위한 상태
   const [showInfo, setShowInfo] = useState(false);
-  const [selectedPod, setSelectedPod] = useState<string | null>(null);
-  const [selectedEdge, setSelectedEdge] = useState<string | null>(null);
-  const podData: Data[] = data;
+  const [selectedPod, setSelectedPod] = useState<JSX.Element | null>(null);
+  const [selectedEdge, setSelectedEdge] = useState<JSX.Element | null>(null);
 
-  // 노드 위치 및 색상을 관리하는 상태 추가
+  // 노드 위치, 색상 및 데이터 관리를 위한 상태
   const [nodePositions, setNodePositions] = useState<{ [key: string]: { x: number; y: number } }>({});
   const [nodeColors, setNodeColors] = useState<{ [key: string]: string }>({});
+  const [nodes, setNodes] = useState<{ x: number; y: number; name: string; size: number }[]>([]);
+  const [links, setLinks] = useState<{ source: string; target: string }[]>([]);
+  const [groupedNodes, setGroupedNodes] = useState<{ [key: string]: number }>({});
 
-  const nodeGroups: { [key: string]: number } = {}; // 같은 src를 가진 것끼리 묶음
-  const nodes: { x: number; y: number; name: string; size: number }[] = [];
-  const links: { source: string; target: string }[] = [];
+  // data.json 데이터 로드
+  const podData: Data[] = data;
 
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
-  // 그래프의 크기 설정
+  // 그래프 크기 정의
   const graphWidth = 1300;
   const graphHeight = 610;
 
-  for (const pod of podData) {
-    const nodeKey = `${pod.src_ip}:${pod.src_port}`;
-    if (!nodePositions[nodeKey]) { //노드 위치 고정
-      const position = {
-        x: Math.random() * graphWidth*3,
-        y: Math.random() * graphHeight,
-      };
-      setNodePositions((prevPositions) => ({ ...prevPositions, [nodeKey]: position }));
+  useEffect(() => {
 
-      if (!nodeColors[nodeKey]) { //노드 색상 고정
-        const color = getRandomColor();
-        setNodeColors((prevColors) => ({ ...prevColors, [nodeKey]: color }));
+    // 노드 색 랜덤 지정
+    const getRandomColor = () => {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
       }
-    }
-    if (nodeGroups[nodeKey]) {
-      nodeGroups[nodeKey]++;
-    } else {
-      nodeGroups[nodeKey] = 1;
-    }
+      return color;
+    };
 
-    const position = nodePositions[nodeKey];
-    if (position) { // 노드 존재하는지 확인
-      nodes.push({
+    //노드 및 간선 생성 변수
+    const groupedNodes: { [key: string]: number } = {};
+    const newNodes: { x: number; y: number; name: string; size: number }[] = [];
+    const newLinks: { source: string; target: string }[] = [];
+
+    //노드 생성 함수
+    const createNode = (dstPodKey: string) => {
+      const padding = 100; 
+      const position = {
+        x: Math.random() * (graphWidth + padding) * 3,
+        y: Math.random() * (graphHeight + padding),
+      };
+    
+      groupedNodes[dstPodKey] = 1;
+    
+      if (!nodePositions[dstPodKey]) {
+        const color = getRandomColor();
+        setNodePositions((nodePositions) => ({ ...nodePositions, [dstPodKey]: position }));
+        setNodeColors((nodeColors) => ({ ...nodeColors, [dstPodKey]: color }));
+        setGroupedNodes((groupedNodes) => ({ ...groupedNodes, [dstPodKey]: groupedNodes[dstPodKey] + 1 }));
+      }
+    
+      return {
         x: position.x,
         y: position.y,
-        name: nodeKey,
-        size: nodeGroups[nodeKey] * 10,
-      });
-    }
+        name: dstPodKey,
+        size: 100,
+      };
+    };
 
-    // 간선 그리기
-    const targetNodeKey = `${pod.dst_ip}:${pod.dst_port}`;
-    const targetPosition = nodePositions[targetNodeKey];
-    if (targetPosition) { // target이 존재하는지 확인
-      nodes.push({
-        x: targetPosition.x,
-        y: targetPosition.y,
-        name: targetNodeKey,
-        size: nodeGroups[targetNodeKey] * 10,
-      });
-      links.push({ source: nodeKey, target: targetNodeKey });
-    }
-  }
+    podData.forEach((pod) => {
+      const srcNodeKey = `${pod.src_ip}:${pod.src_port}`;
+      const targetNodeKey = `${pod.dst_ip}:${pod.dst_port}`;
+    
+      //src 노드가 존재하지 않으면 생성
+      if (!groupedNodes[srcNodeKey]) {
+        newNodes.push(createNode(srcNodeKey));
+      }
+    
+      //dst 노드가 존재하지 않으면 생성
+      if (!groupedNodes[targetNodeKey]) {
+        newNodes.push(createNode(targetNodeKey));
+      }
+    
+      newLinks.push({ source: srcNodeKey, target: targetNodeKey });
+    });
 
-  // 뷰포트
-  const [viewport, setViewport] = useState({ 
-    x: 0,
-    y: 0,
-  });
-  
-  // 마우스 드래그 핸들러
-  const handleMouseMove = (e: React.MouseEvent) => {
-    if (e.buttons === 1) { 
-      const dx = e.movementX;
-      const dy = e.movementY;
-  
-      setViewport((prevViewport) => ({
-        x: prevViewport.x - dx,
-        y: prevViewport.y + dy, 
-      }));
-    }
-  };
+    // 상태 업데이트
+    setNodes(newNodes);
+    setLinks(newLinks);
+  }, [podData, nodePositions, nodeColors]);
 
-  // 파드(원) 클릭 핸들러
-  const handlePodClick = (pod: { x: number; y: number; name: string }) => {
-    setSelectedPod(`${pod.name}\nsrc_ip: ${pod.name.split(':')[0]}\nsrc_port: ${pod.name.split(':')[1]}`);
-    setSelectedEdge(null);
-    setShowInfo(true);
-  };
-
-  //간선(화살표) 클릭 핸들러
-  const handleArrowClick = (sourcePod: string, destPod: string) => {
-    setSelectedPod(`Communication from ${sourcePod} to ${destPod}`);
-    const edgeData = findEdgeData(sourcePod, destPod);
-    setSelectedEdge(
-      `dst_ip: ${edgeData.dst_ip}\ndst_port: ${edgeData.dst_port}\ndata_len: ${edgeData.data_len}\nprotocol: ${edgeData.protocol}`
-    );
-    setShowInfo(true);
-  };
-
-  //통신 노드 찾기 
+  // 엣지 데이터 검색
   const findEdgeData = (sourcePod: string, destPod: string) => {
     const edgeData = podData.find(
       (pod) =>
@@ -136,53 +112,112 @@ const Operation: React.FC = () => {
     return edgeData || { dst_ip: '', dst_port: '', data_len: '', protocol: '' };
   };
 
-  //원 그리기
+  // 노드 렌더링
   const renderMarkSeries = () => {
     return nodes.map((node) => (
-      <MarkSeries
-        key={node.name}
-        data={[node]}
-        fill={nodeColors[node.name]} 
-        stroke="none"
-        sizeRange={[0, 100]} 
-        onValueClick={() => handlePodClick(node)}
-      />
-    ));
-  };
+        <MarkSeries
+          key={node.name}
+          data={[node]}
+          fill={nodeColors[node.name]}
+          stroke="none"
+          sizeRange={[0, (groupedNodes[node.name] || 1) * 100]}
+          onValueClick={() => handlePodClick(node)}
+        />
+     ));
+   };
 
-  //간선 그리기
+  // 엣지 렌더링
   const renderLineSeries = () => {
     return links.map((link, index) => (
-      <LineSeries
-        key={index}
-        data={[
-          nodes.find((n) => n.name === link.source)!,
-          nodes.find((n) => n.name === link.target)!,
-        ]}
-        onValueClick={() => handleArrowClick(link.source, link.target)}
-        stroke={10}
-      />
+        <LineSeries
+          key={index}
+          data={[
+            nodes.find((n) => n.name === link.source)!,
+            nodes.find((n) => n.name === link.target)!,
+          ]}
+          style={{stroke: 'black', strokeWidth: 5 }} 
+          onSeriesClick={() => handleEdgeClick(link)} 
+        /> 
     ));
   };
+  
+  // 뷰포트 상태 업데이트
+  const [viewport, setViewport] = useState({
+    x: 0,
+    y: 0,
+  });
 
+  // 마우스 이동 핸들러
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (e.buttons === 1) {
+      const dx = e.movementX;
+      const dy = e.movementY;
+
+      setViewport((prevViewport) => ({
+        x: prevViewport.x - dx,
+        y: prevViewport.y + dy,
+      }));
+    }
+  };
+
+  // 노드 클릭 핸들러
+  const handlePodClick = (pod: { x: number; y: number; name: string }) => {
+    setSelectedPod(
+      <div>
+        <h3>Pod Information</h3>
+        <p>
+          <VscCircleSmall /> IP: {pod.name.split(':')[0]} <br />
+          <VscCircleSmall /> Port: {pod.name.split(':')[1]}{' '}
+        </p>
+      </div>
+    );
+    setSelectedEdge(null);
+    setShowInfo(true);
+  };
+
+  // 엣지 클릭 핸들러
+  const handleEdgeClick = (edge: { source: string; target: string }) => {
+    const edgeData = findEdgeData(edge.source, edge.target);
+    setSelectedEdge(
+      <div>
+        <h3>Communication Information</h3>
+        <p>
+          <VscCircleSmall /> Communication {edge.source} to {edge.target} <br />
+          <VscCircleSmall /> Dst IP: {edgeData.dst_ip} <br />
+          <VscCircleSmall /> Dst Port: {edgeData.dst_port} <br />
+          <VscCircleSmall /> Data length: {edgeData.data_len} <br />
+          <VscCircleSmall /> Protocol: {edgeData.protocol}{' '}
+        </p>
+      </div>
+    );
+    setSelectedPod(null);
+    setShowInfo(true);
+  };
+  
   return (
     <div className='content' onMouseMove={handleMouseMove}>
-        <XYPlot width={graphWidth} height={graphHeight} style={{ marginTop: '30px' }} xDomain={[viewport.x, viewport.x + 3000]} yDomain={[viewport.y, viewport.y + 610]}>
-          {renderMarkSeries()}
-          {renderLineSeries()}
-        </XYPlot>
-        {showInfo && (
-          <div className='info-box'>
-            <div className='info-content'>
-              <button onClick={() => setShowInfo(false)} className='info-top'>
-                <b>Details</b>
-                <VscExport />
-              </button>
-              <p className='metadata'>{selectedPod}</p>
-              {selectedEdge && <p className='metadata'>{selectedEdge}</p>}
-            </div>
+      <XYPlot
+        width={graphWidth}
+        height={graphHeight}
+        style={{ marginTop: '30px' }}
+        xDomain={[viewport.x, viewport.x + 3000]}
+        yDomain={[viewport.y, viewport.y + 610]}
+      >
+        {renderMarkSeries()}
+        {renderLineSeries()}
+      </XYPlot>
+      {showInfo && (
+        <div className='info-box'>
+          <div className='info-content'>
+            <button onClick={() => setShowInfo(false)} className='info-top'>
+              <VscExport />
+              <b>Details</b>
+            </button>
+            <p className='metadata'>{selectedPod}</p>
+            {selectedEdge && <p className='metadata'>{selectedEdge}</p>}
           </div>
-        )}
+        </div>
+      )}
     </div>
   );
 };
