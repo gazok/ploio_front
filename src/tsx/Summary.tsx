7f5401a4997da7ea32712e3dfb33a9fcf5b2861f
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-12-13 07:19:42 +0900

    api 명세에 맞게 수정
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

--- src/tsx/Summary.tsx
+++ src/tsx/Summary.tsx
@@ -5,12 +5,11 @@ import React, { useEffect, useRef, useState } from "react";
 import "../css/App.css";
 import "../css/Summary.css";
 import { Link, Outlet } from "react-router-dom";
-import { VscSync, VscShield, VscSearch, VscZoomOut, VscZoomIn } from "react-icons/vsc";
-import { Data, JsonData, PodData, PodJsonData, SecurityData, SecurityJsonData } from "./types.tsx";
+import { Data, JsonData, PodData, PodJsonData, NoticeData, NoticeJsonData } from "./types";
 import data from '../public/data.json';
 import data2 from '../public/data2.json';
 import data3 from '../public/data3.json';
-
+import { json } from "d3";
 
 const iconStyle: React.CSSProperties = {
   marginRight: '8px',
@@ -26,85 +25,50 @@ function SummaryTop() {
   return (
     <nav className="summary-top">
       <Link to="Operation" className={` ${activeButton === 'operation' ? 'active-summary' : ''}`} onClick={() => handleButtonClick('operation')}>
-        <VscSync style={iconStyle} />
         Operation
       </Link>
-      <Link to="Security" className={` ${activeButton === 'security' ? 'active-summary' : ''}`} onClick={() => handleButtonClick('security')}>
-        <VscShield style={iconStyle} />
-        Security
-      </Link>
     </nav>
-  );
+  );/*<Link to="Security" className={` ${activeButton === 'security' ? 'active-summary' : ''}`} onClick={() => handleButtonClick('security')}>
+  <VscShield style={iconStyle} />
+  Security
+</Link>*/
 }
 
-export const Logic = async (callback: (data: JsonData | null) => void) => {
-  //callback(null);
-    //callback(data);
-    
-    //'http://serverIp:Port/Path'
-    const res: JsonData = await fetch('../public/data.json', { // /summary/{edgeid}로 바꾸기.
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-    }).then(response => response.json());
-    callback(res);
+export const Logic = async (callback: (packets: JsonData | null) => void, callback2: (data: any) => void, callback3: (data: any) => void) => { // callback2: (ns_podname: string, pod_data: any)
 
-    const res2: PodJsonData = await fetch('../public/data.json2', { // /summary/{edgeid}로 바꾸기.
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-    }).then(response => response.json());
-    //console.log(res);
-    //console.log(res2);
+  callback(data);
 
-    const res3: SecurityJsonData = await fetch('../public/data.json2', { // /summary/{edgeid}로 바꾸기.
-      method: 'GET',
-      headers: {'Content-Type': 'application/json'}
-  }).then(response => response.json());
+  const curPodData = new Map();
+  const curNoticeData = new Map(); 
 
-    //const temp: PodData[] = [];
-    // const curPodData = podData;
-    const curPodData = new Map();
-    const curSecurityData = new Map(); 
-    // res.data.forEach((item: Data) => {
-    //     //console.log(res2[item.src_pod]);
-    //     //temp.push(res2[item.src_pod]);
-    //     //temp.push(res2[item.dst_pod]);
+  // for(let key in data2) {
+  //   const mapkey = `${data2[key].name_space}:${data2[key].name}`;
+  //   curPodData.set(mapkey, data2[key]);
+  // }
+  
+  //수정
+  for(let key in data2.pods) {
+    for(let serverName in data2.pods[key]) {
+      curPodData.set(serverName, data2.pods[key][serverName]);
+    }
+  }
 
-    //     curPodData.set(item.src_pod, res2[item.src_pod]);
-    //     curPodData.set(item.dst_pod, res2[item.dst_pod]);
-    // });
+  data3.notices.forEach(item => {
+    const mapkey = `${item.src_pod}${item.dst_pod}`;
+    curNoticeData.set(mapkey, item);
+  });
+  
+  callback2(curPodData);
+  callback3(curNoticeData);
 
-    res2.data.forEach((item: PodData) => {
-      const key = `${item.name_space}-${item.name}`;
-      curSecurityData.set(key, item);
-    });
-
-    res3.data.forEach((item: SecurityData) => {
-        const key = `${item.src_pod}-${item.dst_pod}`;
-        curSecurityData.set(key, item);
-    });
-
-    //callback2(temp);
-    return { curPodData, curSecurityData }
+  return;
 
 }
-
-// export const Logic = async (callback: (data: JsonData | null) => void, podData: Map<any, any>, callback2: (plz: any) => void) => {
-//   const res: JsonData = await fetch('/data.json').then(response => response.json());
-//   callback(res);
-
-//   const res2 = await fetch('/data2.json').then(response => response.json());
-
-//   const curPodData = podData;
-//   res.data.forEach((item: Data) => {
-//       curPodData.set(item.src_pod, res2[item.src_pod]);
-//       curPodData.set(item.dst_pod, res2[item.dst_pod]);
-//   });
-// }
 
 
 export const LogicPod = async (pn: string, callback: (ns_podname: string, pod_data: any) => void) => {
   //callback(null);
-  const pod_data = data2["default:A2"]; // data2 바꾸고 이것도 바꾸기!
+  const pod_data = data2["default:api-server"]; // data2 바꾸고 이것도 바꾸기!
   callback(pn, pod_data);
   //Pod name 보내면 가져오는 식
   /*
