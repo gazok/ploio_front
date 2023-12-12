89a7771aa5544c7102aabad53f0d027a3956206f
HANISJS <99195207+HANISJS@users.noreply.github.com>
2023-12-13 07:01:35 +0900

    Update AppRoutes.tsx
    Signed-off-by: HANISJS <99195207+HANISJS@users.noreply.github.com>

--- src/tsx/AppRoutes.tsx
+++ src/tsx/AppRoutes.tsx
@@ -6,22 +6,23 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Login from "./login.tsx";
 import { Signup } from "./signup.tsx";
 import Summary from "./summary.tsx";
-import Notice from "./Notice.tsx";
-import NOps from "./NOps.tsx";
-import NSec from "./NSec.tsx";
+import { Notice, NoticeM } from "./Notice.tsx";
+// import NOps from "./NOps.tsx";
+// import NSec from "./NSec.tsx";
 import Profile from "./Profile.tsx";
 import Setting from "./Setting.tsx";
 //import Security from "./Security";
-import Operation from "./Operating.tsx";
+import {Operation, OperationM} from "./Operation.tsx";
+import { Management, ManagementM } from "./Management.tsx";
 import { Top, Menubar, Content } from "./background.tsx";
 import "../css/App.css";
 import { createTheme, ThemeProvider } from '@fluentui/react';
 
-const myTheme = createTheme({ //폰트 설정
-  defaultFontStyle: { fontFamily: 'Comfortaa', fontWeight: 'regular' },
+const myTheme = createTheme({ //폰트 설정, 수정
+  defaultFontStyle: { fontFamily: 'Segoe UI', fontWeight: 'regular' },
   fonts: {
     medium: {
-      fontFamily: 'Comfortaa',
+      fontFamily: 'Segoe UI',
       fontWeight: 'regular',
       fontSize: '15px',
     },
@@ -36,18 +37,20 @@ function AppRoutes() {
       <BrowserRouter>
         <Menubar />
         <Routes>
-          <Route path="/Login" element={<div><Top /><Login /></div>} />
+          <Route path="/Signin" element={<div><Top /><Login /></div>} />
           <Route path="/Signup" element={<div><Top /><Signup /></div>} />
           <Route path="/Summary" element={<Summary />}>
-            <Route index element={<div><Operation /></div>} />
-            <Route path="Operation" element={<div><Operation /></div>} />
+            <Route index element={<div><OperationM /></div>} />
+            <Route path="Operation" element={<div><OperationM /></div>} />
 
           </Route>
-          <Route path="/Notice" element={<Notice />}>
-            <Route index element={<NOps />} />
+          <Route path="/Notice" element={<NoticeM />} /> {/*수정*/}
+            {/* <Route index element={<NOps />} />
             <Route path="NOps" element={<NOps />} />
-            <Route path="NSec" element={<NSec />} />
-          </Route>
+            <Route path="NSec" element={<NSec />} /> */}
+          {/* </Route> */}
+          <Route path="/Management" element={<div><ManagementM /></div>} />
+
           <Route path="/Profile" element={<div><Top /><Profile /></div>} />
           <Route path="/Setting" element={<div><Top /><Setting /></div>} />
           <Route path="/" element={<div><Top /><Content /></div>} />
